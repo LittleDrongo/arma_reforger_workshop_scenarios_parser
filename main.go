fcf656ad1e47b323a1ce5562cc5d7ca98acb30c9
Fomin Alexey <fomin.alexey@outlook.com>
2025-03-05 13:57:06 +0300

    getter

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 )
 
 func main() {
-	printMissions("https://reforger.armaplatform.com/workshop/7777777777777771-PodvalPvP-PostSovietEra/scenarios")
-	// quickPrint("https://reforger.armaplatform.com/workshop/7777777777777771-PodvalPvP-PostSovietEra/scenarios")
-	// quickPrint("https://reforger.armaplatform.com/workshop/7777777777777777-Podval_PVP_Vanilla/scenarios")
-	// quickPrint("https://reforger.armaplatform.com/workshop/5F0ECCEE7B9AA8CE-LobbyMissionsRHS/scenarios")
-	// quickPrint("https://reforger.armaplatform.com/workshop/5EAF1CB52EF38F0D-ReforgerLobbyMissions/scenarios")
+	// printMissions("https://reforger.armaplatform.com/workshop/7777777777777771-PodvalPvP-PostSovietEra/scenarios")
+	quickPrintSample("https://reforger.armaplatform.com/workshop/7777777777777771-PodvalPvP-PostSovietEra/scenarios")
+	quickPrintSample("https://reforger.armaplatform.com/workshop/7777777777777777-Podval_PVP_Vanilla/scenarios")
+	quickPrintSample("https://reforger.armaplatform.com/workshop/5F0ECCEE7B9AA8CE-LobbyMissionsRHS/scenarios")
+	quickPrintSample("https://reforger.armaplatform.com/workshop/5EAF1CB52EF38F0D-ReforgerLobbyMissions/scenarios")
 }
 
-func quickPrint(url string) {
+func quickPrintSample(url string) {
 	addon, err := worshop.GetAddonsDataFromWorkshop(url)
 	if err != nil {
 		log.Fatalf("err: %v", err)
@@ -25,7 +25,7 @@ func quickPrint(url string) {
 	fmt.Println()
 }
 
-func printMissions(url string) {
+func printMissionsSample(url string) {
 	addon, err := worshop.GetAddonsDataFromWorkshop(url)
 	if err != nil {
 		log.Fatalf("err: %v", err)
