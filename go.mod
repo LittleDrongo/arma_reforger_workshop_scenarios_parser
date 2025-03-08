c4716614c23daf081240517ca2ba0c606dd73504
Fomin Alexey <fomin.alexey@outlook.com>
2025-03-08 11:22:24 +0300

    *

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module workshop_parser
 
 go 1.23.3
 
-require github.com/PuerkitoBio/goquery v1.10.2
+require (
+	github.com/LittleDrongo/fmn-lib v1.0.35
+	github.com/PuerkitoBio/goquery v1.10.2
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.3 // indirect
 	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"log"
 	"strings"
 	"workshop_parser/model/worshop"
+
+	"github.com/LittleDrongo/fmn-lib/console/cmd"
 )
 
 func main() {
-	// printMissions("https://reforger.armaplatform.com/workshop/7777777777777771-PodvalPvP-PostSovietEra/scenarios")
-	// quickPrintSample("https://reforger.armaplatform.com/workshop/7777777777777771-PodvalPvP-PostSovietEra/scenarios")
+	quickPrintSample("https://reforger.armaplatform.com/workshop/7777777777777771-PodvalPvP-PostSovietEra/scenarios")
 	quickPrintSample("https://reforger.armaplatform.com/workshop/7777777777777777-Podval_PVP_Vanilla/scenarios")
-	// quickPrintSample("https://reforger.armaplatform.com/workshop/5F0ECCEE7B9AA8CE-LobbyMissionsRHS/scenarios")
-	// quickPrintSample("https://reforger.armaplatform.com/workshop/5EAF1CB52EF38F0D-ReforgerLobbyMissions/scenarios")
+	quickPrintSample("https://reforger.armaplatform.com/workshop/5F0ECCEE7B9AA8CE-LobbyMissionsRHS/scenarios")
+	quickPrintSample("https://reforger.armaplatform.com/workshop/5EAF1CB52EF38F0D-ReforgerLobbyMissions/scenarios")
+
+	cmd.Input("Нажмите Enter чтобы закрыть приложение.")
 }
 
 func quickPrintSample(url string) {
